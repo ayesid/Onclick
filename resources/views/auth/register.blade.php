c2982ba74373e233c8d468f26cd352dce07f7e5b
kirito <nelsonalba67@gmail.com>
2024-09-18 18:56:37 -0500

    login terminado

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,15 +4,64 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-label for="name" :value="__('nombre')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+                autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
-
+        <!-- segundo nombre -->
+        <div>
+            <x-input-label for="senombre" :value="__('Segundo nombre')" />
+            <x-text-input id="senombre" class="block mt-1 w-full" type="text" name="senombre" :value="old('senombre')"
+                required autofocus autocomplete="senombre" />
+            <x-input-error :messages="$errors->get('senombre')" class="mt-2" />
+        </div>
+        <!-- apellido -->
+        <div>
+            <x-input-label for="apellido" :value="__('apellido')" />
+            <x-text-input id="apellido" class="block mt-1 w-full" type="text" name="apellido" :value="old('apellido')"
+                required autofocus autocomplete="apellido" />
+            <x-input-error :messages="$errors->get('apellido')" class="mt-2" />
+        </div>
+        <!-- segundo apellido -->
+        <div>
+            <x-input-label for="seapellido" :value="__('segundo apellido')" />
+            <x-text-input id="seapellido" class="block mt-1 w-full" type="text" name="seapellido" :value="old('seapellido')"
+                required autofocus autocomplete="seapellido" />
+            <x-input-error :messages="$errors->get('seapellido')" class="mt-2" />
+        </div>
+        <!-- telefono -->
+        <div>
+            <x-input-label for="telefono" :value="__('telefono')" />
+            <x-text-input id="telefono" class="block mt-1 w-full" type="text" name="telefono" :value="old('telefono')"
+                required autofocus autocomplete="telefono" />
+            <x-input-error :messages="$errors->get('telefono')" class="mt-2" />
+        </div>
+        <!-- direccion -->
+        <div>
+            <x-input-label for="direccion" :value="__('direccion')" />
+            <x-text-input id="direccion" class="block mt-1 w-full" type="text" name="direccion" :value="old('direccion')"
+                required autofocus autocomplete="direccion" />
+            <x-input-error :messages="$errors->get('direccion')" class="mt-2" />
+        </div>
+        <!-- municipios -->
+        <div>
+            <x-input-label for="municipio_id" :value="__('Municipio')" />
+            <select id="municipio_id" name="municipio_id" class="block mt-1 w-full" required autofocus>
+                <option value="">Selecciona un municipio</option>
+                @foreach ($municipios as $municipio)
+                    <option value="{{ $municipio->id }}" {{ old('municipio_id') == $municipio->id ? 'selected' : '' }}>
+                        {{ $municipio->nombre }}
+                    </option>
+                @endforeach
+            </select>
+            <x-input-error :messages="$errors->get('municipio_id')" class="mt-2" />
+        </div>
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -20,10 +69,8 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -32,22 +79,22 @@
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
 
             <x-primary-button class="ms-4">
                 {{ __('Register') }}
             </x-primary-button>
-            
+
         </div>
     </form>
 </x-guest-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
 		<link href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">		
 		
 	
-		
+		 
 
 </head>
 
@@ -35,18 +35,377 @@
       </button>
       <div class="collapse navbar-collapse justify-content-center" id="navbarsFurni">
         <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0">
-          <li><a class="nav-link" href="{{ route('login') }}"><img src={{ asset('/images/user.svg') }}>  Iniciar Sesion  </a></li>
-          <li><a class="nav-link" href="{{ route('register') }}">Registrate</a></li>
+          <li><a class="nav-link" href="{{ route('home') }}">Inicio</a></li>
+          <li><a class="nav-link" href="{{ route('centroDeportivo.listar') }}">Centros Deportivos</a></li>
+
+          <li><a class="nav-link" href="{{route('Nosotros')}}">Acerca De Nosotros</a></li>
+          <li><a class="nav-link" href="{{route('Servicios')}}">Servicios</a></li>
+          <li><a class="nav-link" href="{{route('Contactanos')}}">Contactanos</a></li>
+          
+          <li><a class="nav-link" href="{{ route('login') }}"><img src={{ asset('/images/user.svg') }}></a></li>
+          {{-- <li><a class="nav-link" href="{{ route('register') }}">Registrate</a></li> --}}
         </ul>
       </div>
       
     </div>
   </nav>
-
+{{-- 
     <source srcset="sourceset" type="image/svg+xml" />
     <img width="100%" height="100%" src="{{asset('img/cancha_inicio.webp')}} " class="img-fluid" alt="image desc"/>
-  
+   --}}
   
         
+
+		<!-- comienza el carrusel Banner Hero -->
+		<div id="template-mo-zay-hero-carousel" class="carousel slide" data-bs-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="0" class="active"></li>
+				<li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="1"></li>
+				<li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<div class="container">
+						<div class="row p-5">
+							<div class="mx-auto col-md-8 col-lg-6 order-lg-last">
+								<img class="img-fluid" src="img/9-removebg-preview.png" alt="" width="500px">
+							</div>
+							<div class="col-lg-6 mb-0 d-flex align-items-center">
+								<div class="text-align-left align-self-center">
+									<img class="img-fluid" src="img/logo-removebg-preview.png" alt="" width="100px">
+
+									<h1 class="h1 text-success"><b>ONCLICK</b> <br>"Sinteticas a un click"</h1>
+									<h3 class="h2">facil y seguro</h3>
+									<p>
+										Con esta app ahora podras reservar canchas sintéticas de fútbol  
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<div class="container">
+						<div class="row p-5">
+							<div class="mx-auto col-md-8 col-lg-6 order-lg-last">
+								<img class="img-fluid" src="img/1-removebg-preview.png" alt="" style="height: 400px;">
+							</div>
+							<div class="col-lg-6 mb-0 d-flex align-items-center">
+								<div class="text-align-left">
+									<h1 class="h1 text-success">Reserva tu cancha sin complicaciones </h1>
+									<h3 class="h2">Aqui encontraras todos los centros deportivos mas cercanos y con disponibilidad</h3>
+									<p>
+										Reserva tu cancha en tu <strong>centro deportivo </strong >de preferencia
+										
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<div class="container">
+						<div class="row p-5">
+							<div class="mx-auto col-md-8 col-lg-6 order-lg-last">
+								<img class="img-fluid" src="img/5-removebg-preview.png" alt="" style="height: 400px;"  >
+							</div>
+							<div class="col-lg-6 mb-0 d-flex align-items-center">
+								<div class="text-align-left">
+									<h1 class="h1 text-success">Reserva tu cancha rápido, fácil y paga como quieras</h1>
+									<h3 class="h2">Solo con Onclick encuentra los mejores precios y promociones </h3>
+									<p>
+										“Reservamos con Onclick  y nos ahorramos el tener que ir hasta la cancha.”
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<a class="carousel-control-prev text-decoration-none w-auto ps-3" href="#template-mo-zay-hero-carousel"
+				role="button" data-bs-slide="prev">
+				<i class="fas fa-chevron-left"></i>
+			</a>
+			<a class="carousel-control-next text-decoration-none w-auto pe-3" href="#template-mo-zay-hero-carousel"
+				role="button" data-bs-slide="next">
+				<i class="fas fa-chevron-right"></i>
+			</a>
+		</div>
+<!-- fin carrusel Hero Section -->
+
+		
+
+
+		
+
+		<!-- Start Product Section -->
+		<div class="product-section">
+			<div class="container">
+				<div class="row">
+
+					<!-- Start Column 1 -->
+					<div class="col-md-12 col-lg-3 mb-5 mb-lg-0 ">
+						<h2 class="mb-4 section-title">Eventos y promociones </h2>
+						<p class="mb-4">Contamos con Canchas Sintèticas para que disfrutes con tu grupo de amigos. Disponibles para alquiler por horas, torneos y eventos en general. </p>
+						<p><a href="{{ route('centroDeportivo.listar') }}" class="btn btn-primary">Reservar cancha</a></p>
+					</div> 
+					<!-- End Column 1 -->
+
+					<!-- Start Column 2 -->
+					<div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+						<a class="product-item" href="{{route('Servicios')}}">
+							<img src="img/promocion1.jpg" class="img-fluid product-thumbnail">
+							<h3 class="product-title">Promo 1</h3>
+							<strong class="product-price">$50%</strong>
+
+							<span class="icon-cross">
+								<img src="images/cross.svg" class="img-fluid">
+							</span>
+						</a>
+					</div> 
+					<!-- End Column 2 -->
+
+					<!-- Start Column 3 -->
+					<div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+						<a class="product-item" href="{{route('Servicios')}}">
+							<img src="img/promo2.jpg" class="img-fluid product-thumbnail">
+							<h3 class="product-title">Promo 2</h3>
+							<strong class="product-price"> Puntos</strong>
+
+							<span class="icon-cross">
+								<img src="images/cross.svg" class="img-fluid">
+							</span>
+						</a>
+					</div>
+					<!-- End Column 3 -->
+
+					<!-- Start Column 4 -->
+					<div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+						<a class="product-item" href="{{route('Servicios')}}">
+							<img src="img/promo3.jpg" class="img-fluid product-thumbnail">
+							<h3 class="product-title">Promo 3</h3>
+							<strong class="product-price">Cliente del mes</strong>
+
+							<span class="icon-cross">
+								<img src="images/cross.svg" class="img-fluid">
+							</span>
+						</a>
+					</div>
+					<!-- End Column 4 -->
+
+				</div>
+			</div>
+		</div>
+		<!-- End Product Section -->
+
+		<!-- Start Why Choose Us Section -->
+		<div class="why-choose-section">
+			<div class="container">
+				<div class="row justify-content-between">
+					<div class="col-lg-6">
+						<h2 class="section-title">Por qué elegirnos </h2>
+						<p>Nuestra aplicación está diseñada para ser intuitiva y fácil de usar tanto para los usuarios que reservan las canchas como para los administradores de las instalaciones</p>
+
+						<div class="row my-5">
+							
+
+							<div class="col-6 col-md-6">
+								<div class="feature">
+									<div class="icon">
+										<img src="images/bag.svg" alt="Image" class="imf-fluid">
+									</div>
+									<h3>Facil de pagar</h3>
+									<p>Seguro, facil y rapido .</p>
+								</div>
+							</div>
+
+							<div class="col-6 col-md-6">
+								<div class="feature">
+									<div class="icon">
+										<img src="images/support.svg" alt="Image" class="imf-fluid">
+									</div>
+									<h3>24/7 </h3>
+									<p>A cualquier hora del dia puedes reservar</p>
+								</div>
+							</div>
+
+							
+
+						</div>
+					</div>
+
+					<div class="col-lg-5">
+						<div class="img-wrap">
+							<img src="img/can2.jpg" alt="Image" class="img-fluid" style="width: 600px; height: 400px;">
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<!-- End Why Choose Us Section -->
+
+		<!-- Start We Help Section -->
+		<div class="we-help-section">
+			<div class="container">
+				<div class="row justify-content-between">
+					<div class="col-lg-7 mb-5 mb-lg-0">
+						<div class="imgs-grid">
+							<div class="grid grid-1"><img src="img/cancha_inicio.webp" alt="Untree.co" style="width: 500px; height: 500px;"></div>
+							<div class="grid grid-2"><img src="img/2.cancha.jfif" alt="Untree.co" ></div>
+							<div class="grid grid-3"><img src="img/10-Cancha.jpg" alt="Untree.co" style="width: 400px; height: 300px;"></div>
+						</div>
+					</div>
+					<div class="col-lg-5 ps-lg-5">
+						<h2 class="section-title mb-4">"Explora nuestras instalaciones, revisa la disponibilidad en tiempo real y asegura tu espacio para jugar."</h2>
+						<p>"Nuestras canchas sintéticas están ubicadas en un entorno seguro y accesible, equipadas con iluminación nocturna para que puedas jugar en cualquier momento del día."</p>
+
+						<ul class="list-unstyled custom-list my-4">
+							<li>"Selecciona la fecha y la hora que prefieras, y reserva tu cancha en solo unos clics. Recibirás una confirmación instantánea por correo electrónico."</li>
+							<li>"Tarifas competitivas y opciones de pago flexibles disponibles. ¡Reserva ahora y prepárate para disfrutar de tu juego!"</li>
+							<li>"Por favor, llega a tiempo para tu reserva y asegúrate de respetar las reglas y normativas de nuestras instalaciones."</li>
+							<li>"Cancelaciones y reembolsos: Consulta nuestras políticas de cancelación para obtener información sobre cómo modificar o cancelar tu reserva."</li>
+						</ul>
+					
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End We Help Section -->
+
+		<!-- Start Popular Product -->
+		<div class="popular-product">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
+						<div class="product-item-sm d-flex">
+							<div class="thumbnail">
+								<img src="img/3-removebg-preview.png" alt="Image" class="img-fluid">
+							</div>
+							<div class="pt-3">
+								<h3>Intuitiva</h3>
+								<p>Onclick - Reserva de canchas sintéticas en Popayán de forma intuitiva y rápida.  </p>
+								
+							</div>
+						</div>
+					</div>
+
+					<div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
+						<div class="product-item-sm d-flex">
+							<div class="thumbnail">
+								<img src="img/jugadorpng-removebg-preview.png" alt="Image" class="img-fluid">
+							</div>
+							<div class="pt-3">
+								<h3>Facil de usar</h3>
+								<p>Nuestra app es muy facil y divertida de usar </p>
+								
+							</div>
+						</div>
+					</div>
+
+					<div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
+						<div class="product-item-sm d-flex">
+							<div class="thumbnail">
+								<img src="img/jugadpng2-removebg-preview.png" alt="Image" class="img-fluid">
+							</div>
+							<div class="pt-3">
+								<h3>Rapida</h3>
+								<p>Nuestra app está diseñada para que puedas encontrar y reservar la cancha perfecta en segundos. </p>
+								
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<!-- End Popular Product -->
+	
+
+		<!-- Start Footer Section -->
+		<footer class="footer-section bg-gradient" >
+			<div class="container relative">
+
+				<div class="sofa-img">
+					<img src="img/dosjuga-removebg-preview.png" alt="Image" class="img-fluid">
+				</div>
+
+				<div class="row">
+					<div class="col-lg-8">
+					  <div class="subscription-form">
+						<h3 class="d-flex align-items-center"><span class="me-1"><img src="images/envelope-outline.svg" alt="Correo" class="img-fluid"></span><span>Subscríbete a nuestro boletín</span></h3>
+			
+						<form action="#" class="row g-3">
+						  <div class="col-auto">
+							<input type="text" class="form-control" placeholder="Ingresa tu correo">
+						  </div>
+						  <div class="col-auto">
+							<button class="btn btn-primary">Subscríbete</button>
+						  </div>
+						</form>
+			
+					  </div>
+					</div>
+				  </div>
+
+				<div class="row g-5 mb-5">
+					<div class="col-lg-4">
+						<div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Onclick<span><div class="hero-img-wrap">
+							<img src="img/logo-removebg-preview.png " class="img-fluid" style="width: 100px;">
+			
+						</div></span></a>
+					</div>
+					<p>Tu mejor opción para reservar canchas sintéticas en Popayán. Fácil, rápido y conveniente.</p>
+
+						<ul class="list-unstyled custom-social">
+							<li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
+							<li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
+							<li><a href="#"><span class="fa fa-brands fa-instagram"></span></a></li>
+							<li><a href="#"><span class="fa fa-brands fa-linkedin"></span></a></li>
+						</ul>
+					</div>
+
+					<div class="col-lg-8">
+						<div class="row links-wrap">
+						  <div class="col-6 col-sm-6 col-md-3">
+							<ul class="list-unstyled">
+							  <li><a href="#">Acerca de Nosotros</a></li>
+							  <li><a href="#">Servicios</a></li>
+					
+							  <li><a href="#">Contacto</a></li>
+							</ul>
+						  </div>
+			  
+						  <div class="col-6 col-sm-6 col-md-3">
+							<ul class="list-unstyled">
+							  <li><a href="#">Términos y Condiciones</a></li>
+							  <li><a href="#">Política de Privacidad</a></li>
+							  <li><a href="#">Soporte</a></li>
+							</ul>
+						  </div>
+						</div>
+					  </div>
+
+				</div>
+
+				<div class="border-top copyright">
+					<div class="row pt-4">
+					  <div class="col-lg-6">
+						<p class="mb-2 text-center text-lg-start">© 2024 Onclick Reservas. Todos los derechos reservados.</p>
+					  </div>
+			
+					  <div class="col-lg-6 text-center text-lg-end">
+						<ul class="list-unstyled d-inline-flex ms-auto">
+						  <li class="me-4"><a href="#">Términos</a></li>
+						  <li class="me-4"><a href="#">Privacidad</a></li>
+						  <li><a href="#">Contacto</a></li>
+						</ul>
+					  </div>
+					</div>
+				  </div>
+
+			</div>
+		</footer>
     </body>
 </html>
